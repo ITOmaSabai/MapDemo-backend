d7cda9b6c9ebaecfe4b23515b31529797c26db8b
ItoSbai <lualaa.design2565@gmail.com>
2024-02-22 23:32:27 +0900

    ファイルをbackendディレクトリに移動

---
+++ app/controllers/api/v1/maps_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::MapsController < ApplicationController
+  def index
+    # @maps = Map.where.not(lat: nil, lng: nil)
+    # render json: @maps
+  end
+
+  def show
+    # @map = Map.includes(:videos).find(params[:id])
+    # @videos = @map.videos
+  end
+end
