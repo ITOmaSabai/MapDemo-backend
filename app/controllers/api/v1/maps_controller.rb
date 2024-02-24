96fbeee1bd5e1eefbb120ce34bc47c398cd054b9
ItoSbai <lualaa.design2565@gmail.com>
2024-02-24 19:56:14 +0900

    feat:map#createアクションを作成

--- app/controllers/api/v1/maps_controller.rb
+++ app/controllers/api/v1/maps_controller.rb
@@ -1,11 +1,25 @@
 class Api::V1::MapsController < ApplicationController
   def index
-    # @maps = Map.where.not(lat: nil, lng: nil)
-    # render json: @maps
+    @maps = Map.where.not(lat: nil, lng: nil)
+    render json: @maps
   end
 
   def show
-    # @map = Map.includes(:videos).find(params[:id])
-    # @videos = @map.videos
+    @map = Map.includes(:videos).find(params[:id])
+    @videos = @map.videos
+  end
+
+  def create
+    map = Map.new(map_params)
+    if map.save
+      render json: map, status: :created
+    else
+      render json: map.errors, status: :unprocessable_entity
+    end
+
+  private
+
+  def map_params
+    params.require(:map).permit(:name, :description, :lat, :lng)
   end
 end
