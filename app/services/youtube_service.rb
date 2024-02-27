efd1d69f14ae683421b5147bbf9db19a42898f0c
ItoSbai <lualaa.design2565@gmail.com>
2024-02-27 09:42:11 +0900

    feat:YouTube Data APIの検索ロジックを持つserviceクラスを作成

---
+++ app/services/youtube_service.rb
@@ -0,0 +1,21 @@
+require 'google/apis/youtube_v3'
+
+class YoutubeService
+  def initialize(api_key:)
+    @youtube = Google::Apis::YoutubeV3::YouTubeService.new
+    @youtube.key = api_key
+  end
+
+  def search_videos(keyword)
+    @youtube.list_searches(
+        'snippet',
+        q: "#{keyword}",
+        # location: "#{lat},#{lng}",
+        max_results: '10',
+        order: 'viewCount',
+        type: 'video',
+        # videoEmbeddable: 'true'
+        # fields: 'items(id,snippet(title,channel_title,thumbnails))'
+    )
+  end
+end
