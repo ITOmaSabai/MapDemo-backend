b0b74768e5e9140c86564731bb5b69ebf8504a93
ItoSbai <lualaa.design2565@gmail.com>
2024-02-27 09:35:37 +0900

    feat:YouTubeを検索するserviceクラスを作成

---
+++ app/services/video_search_service.rb
@@ -0,0 +1,10 @@
+class VideoSearchService
+  def self.call(:keyword)
+    youtube_service = YoutubeService.new(api_key: ENV['GOOGLE_MAP_API_KEY'])
+    @videos = youtube_service.search_videos(keyword)
+  rescue Google::Apis::Error => e
+    # APIからのエラーを適切に処理する
+    flash[:alert] = '動画の検索中にエラーが発生しました。'
+    redirect_to root_path
+  end
+end
