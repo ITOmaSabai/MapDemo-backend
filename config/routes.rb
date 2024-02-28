315e57c1b92481092eff5e68942cb8658c8eb6da
ItoSbai <lualaa.design2565@gmail.com>
2024-02-28 14:51:43 +0900

    add:home#indexへのルーティングをRails側に追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'home/index'
   namespace :api do
     namespace :v1 do
       resources :maps, only: [:create, :show, :index, :update, :destroy]
       resources :videos, only: [:create, :show, :index, :update, :destroy]
     end
   end
-  
+
+  get '*path', to: 'home#index'
+  root to: 'home#index'
 end
