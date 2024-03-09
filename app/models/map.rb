efd2d53a3aa74250a7c38fceabd60255ef7713eb
ItoSbai <lualaa.design2565@gmail.com>
2024-03-09 16:20:25 +0900

    update:Map取得時に関連するAddressも取得するよう機能を修正
    fix:Mapモデルの関連付けaddressesをaddressに修正

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,6 +1,6 @@
 class Map < ApplicationRecord
   has_many :videos, dependent: :destroy
-  has_one :addresses, dependent: :destroy
+  has_one :address, dependent: :destroy
 
   validates :name, :lat, :lng, presence: true
 end
