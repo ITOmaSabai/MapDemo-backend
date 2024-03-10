1b902083be4132efdca73ebfd483e6a9548504f8
ItoSbai <lualaa.design2565@gmail.com>
2024-03-11 07:38:50 +0900

    fix:videos#searchへのルーティングを修正

--- app/controllers/api/v1/videos_controller.rb
+++ app/controllers/api/v1/videos_controller.rb
@@ -9,26 +9,20 @@ class Api::V1::VideosController < ApplicationController
     redirect_to videos_path # 適切なパスにリダイレクト
   end
 
-def index
-  @video = Video.all
-  render json: @video
-end
+  def index
+    @video = Video.all
+    render json: @video
+  end
 
   def search
-    address_for_video = AddressService.save_address_for_video(1, video_params[:address_components], video_params[:formatted_addres])
+    address_for_video = AddressService.save_address_for_video(1, video_params[:address_components], video_params[:formatted_address])
     videos_data = VideoSearchService.call(keyword: "#{address_for_video[0]} #{address_for_video[1]} walking tour")
-
-    videos_data.items.each do |video_data|
-      youtube_video_id: video_data.id.video_id,
-      thumbnail_url: video_data.snippet.thumbnails.high.url
-    end
-
     render json: videos_data
   end
 
   private
 
   def video_params
-    params.require(:video).permit(:formatted_addres, address_components: [:long_name, :short_name, types: []])
+    params.require(:video).permit(:formatted_address, address_components: [:long_name, :short_name, types: []])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :maps, only: [:create, :show, :index, :update, :destroy]
-      resources :videos, only: [:create, :show, :index, :update, :destroy]
+      resources :videos, only: [:create, :show, :index, :update, :destroy] do
+        post 'search', on: :collection
+      end
       resources :addresses, only: [:index]
     end
   end
-
-  get 'api/v1/videos/search', to: 'videos#search'
 
   get '*path', to: 'home#index'
   root to: 'home#index'
