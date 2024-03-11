7053cf8e2d1f3d7f9aed56737bb8191764bb1b8b
ItoSbai <lualaa.design2565@gmail.com>
2024-03-11 18:14:52 +0900

    update:検索キーワードにlocalityを追加

--- app/controllers/api/v1/videos_controller.rb
+++ app/controllers/api/v1/videos_controller.rb
@@ -17,7 +17,7 @@ class Api::V1::VideosController < ApplicationController
   def search
     address_for_video = AddressService.save_address_for_video(1, video_params[:address_components], video_params[:formatted_address])
     videos_data = VideoSearchService.call(keyword: "#{address_for_video[0]} #{address_for_video[1]} walking tour")
-    search_keywords = ("#{address_for_video[0]} #{address_for_video[1]} walking tour")
+    search_keywords = ("#{address_for_video[0]} #{address_for_video[1]} #{address_for_video[2]} walking tour")
     render json: {videos_data: videos_data, search_keywords: search_keywords}
   end
 
