1b27e5cfb13ff2ae3a301036703f74e1b6aed1e2
ItoSbai <lualaa.design2565@gmail.com>
2024-03-11 09:53:31 +0900

    update:検索キーワードをrenderするように追加

--- app/controllers/api/v1/videos_controller.rb
+++ app/controllers/api/v1/videos_controller.rb
@@ -17,7 +17,8 @@ class Api::V1::VideosController < ApplicationController
   def search
     address_for_video = AddressService.save_address_for_video(1, video_params[:address_components], video_params[:formatted_address])
     videos_data = VideoSearchService.call(keyword: "#{address_for_video[0]} #{address_for_video[1]} walking tour")
-    render json: videos_data
+    search_keywords = ("#{address_for_video[0]} #{address_for_video[1]} walking tour")
+    render json: {videos_data: videos_data, search_keywords: search_keywords}
   end
 
   private
