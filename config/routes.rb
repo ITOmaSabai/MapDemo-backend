28916a2466e220322e99ddf92398f3cca070b73c
ItoSbai <lualaa.design2565@gmail.com>
2024-03-13 12:02:43 +0900

    fix:ルーティングのuser#destroyのタイポを修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
         post 'search', on: :collection
       end
       resources :addresses, only: [:index]
-      resources :users, only: [:index, :create, :destory]
+      resources :users, only: [:index, :create, :destroy]
     end
   end
 
