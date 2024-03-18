fb4222d97d45d995192b012600a0b570a5556790
ItoSbai <lualaa.design2565@gmail.com>
2024-03-18 19:19:21 +0900

    update:likes#indexのルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
         post 'search', on: :collection
       end
       resources :addresses, only: [:index]
-      resources :likes, only: [:create, :destroy]
+      resources :likes, only: [:index, :create, :destroy]
     end
   end
 
