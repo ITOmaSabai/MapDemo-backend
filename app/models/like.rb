b991d4f4e729b1735985a42cdef3d8663f9bf8de
ItoSbai <lualaa.design2565@gmail.com>
2024-03-19 12:16:46 +0900

    fix:バリデーションの対象モデル名を修正

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,5 +2,5 @@ class Like < ApplicationRecord
   belongs_to :map
   belongs_to :user
 
-  validates :user_id, uniqueness: { scope: :spot_id }
+  validates :user_id, uniqueness: { scope: :map_id }
 end
