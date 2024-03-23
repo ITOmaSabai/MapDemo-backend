6c4f3bcd2450d596bb8a85957a8197257761bf1f
ItoSbai <lualaa.design2565@gmail.com>
2024-03-23 16:13:57 +0900

    fix:mapsテーブルのカラムを修正

--- db/migrate/20240323065639_add_ref_to_maps.rb
+++ db/migrate/20240323065639_add_ref_to_maps.rb
@@ -1,4 +1,4 @@
-class AddUserRefToMaps < ActiveRecord::Migration[7.1]
+class AddRefToMaps < ActiveRecord::Migration[7.1]
   def change
     # add_reference :maps, :user, null: false, foreign_key: true
   end
---
+++ db/migrate/20240323070954_add_ref_to_maps2.rb
@@ -0,0 +1,5 @@
+class AddRefToMaps2 < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :maps, :user, null: false, foreign_key: true
+  end
+end
